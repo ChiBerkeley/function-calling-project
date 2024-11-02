41486d3944ef030743657265271716d94c079f7d
Kevin Stallone <kevin@stallone.io>
2024-11-02 02:25:21 -0700

    Made functions a checkbox group, changed some text on app

--- front_end/gradio_front_end.py
+++ front_end/gradio_front_end.py
@@ -8,17 +8,30 @@ demo = gr.Interface(
             ["llama3.1:latest", "llama3.1:8b-instruct-fp16", "llama3.2:1b", "llama3.2:latest", "llama3.2:3b-instruct-fp16"], label="Model", info="Choose a model to generate data."
         ),
         gr.Slider(0.1, 1.0, value=0.5, label="Temperature", info="Choose a temperature for the model."),
-        gr.Dropdown(
+        gr.CheckboxGroup(
             [
-                "calculate_bmi", "get_weather_info", "calculate_area", "get_timezone", 
-                "translate", "calculate_trip_cost", "zodiac_sign", "power", "get_capital", 
-                "count_letter", "sentiment_analysis", "difference_of_squares", "simple_interest", 
-                "password_generator", "date_difference", "calculate_tip", "playlist_duration_calculator"
+                    "calculate_area",
+                    "calculate_bmi",
+                    "calculate_tip",
+                    "calculate_trip_cost",
+                    "count_letter",
+                    "date_difference",
+                    "difference_of_squares",
+                    "get_capital",
+                    "get_timezone",
+                    "get_weather_info",
+                    "password_generator",
+                    "playlist_duration_calculator",
+                    "power",
+                    "sentiment_analysis",
+                    "simple_interest",
+                    "translate",
+                    "zodiac_sign",
             ], label="Function", info="Choose a function to generate data for."
         ),
-        gr.Radio(["True", "False"], label="Randomize", info="Randomize order of output? (not yet implemented)"),
-        gr.Number(label="Examples", info="Number of examples. When using small models, number might not be exact."),
-        gr.Number(label="Runs", info="Number of generations to run. Total output will be examples * runs."),
+        gr.Radio([True, False], value=False, label="Randomize", info="Randomize order of output?"),
+        gr.Number(label="Examples", value=1, info="Number of examples per function. When using small models, number generated might not be exact."),
+        gr.Number(label="Runs",  value=1, info="Number of generations to run. Total output will be number of selected functions * examples * runs. Note, as the number of runs increases, the model may make more mistakes leading to incorrect output or failed structured output."),
     ],
     "json"
 )
