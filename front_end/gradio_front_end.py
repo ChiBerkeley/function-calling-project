7dce21ba14061f2be276b27eb40856a98f8f2ce0
Kevin Stallone <kevin@stallone.io>
2024-11-03 04:55:42 -0800

    Added chat interface & about page and changed theme

--- front_end/gradio_front_end.py
+++ front_end/gradio_front_end.py
@@ -1,9 +1,31 @@
 import gradio as gr
 from generator_for_fe import run
+from langchain_tooluse_for_fe import predict
 
-dark_theme = gr.themes.Soft(primary_hue="purple", secondary_hue="gray", neutral_hue="slate", font=gr.themes.GoogleFont("IBM Plex Sans"))  # Dark theme with appropriate colors
+dark_theme = gr.themes.Soft(
+    primary_hue="purple",
+    secondary_hue="gray",
+    neutral_hue="slate",
+    font=gr.themes.GoogleFont("IBM Plex Sans")
+)
 
-demo = gr.TabbedInterface(
+with gr.Blocks() as about:
+    with gr.Row():
+        with gr.Column():
+            gr.Image(value="front_end/team_photos/chi.jpg", label="Chi So", show_fullscreen_button=False, show_download_button=False)
+            gr.Markdown("Reasearch and implemented evaluation metrics for the pipeline.")
+        with gr.Column():
+            gr.Image(value="front_end/team_photos/edward.png", label="Edward Shin", show_fullscreen_button=False, show_download_button=False)
+            gr.Markdown("Data generation and focused on the frontend design.")
+        with gr.Column():
+            gr.Image(value="front_end/team_photos/kevin.jpg", label="Kevin Stallone", show_fullscreen_button=False, show_download_button=False)
+            gr.Markdown("LangChain structured output for generation pipeline, tool-use chains for trained LLMs, & Gradio front-end implementation.")
+        with gr.Column():
+            gr.Image(value="front_end/team_photos/michael.png", label="Michael Thottam", show_fullscreen_button=False, show_download_button=False)
+            gr.Markdown("SageMaker fine-tuning with generated data for demo LLM focused on Tool Use")
+
+
+app = gr.TabbedInterface(
     [gr.Interface(
         run,
         [
@@ -36,10 +58,13 @@ demo = gr.TabbedInterface(
             gr.Number(label="Runs",  value=1, info="Number of generations to run. Total output will be number of selected functions * examples * runs. Note, as the number of runs increases, the model may make more mistakes leading to incorrect output or failed structured output."),
         ],
         gr.JSON(label="Generated Data"),
-    )],
-    tab_names=["Generate Data", "Chat", "Settings"],
+    ),
+    gr.ChatInterface(predict, type="messages"),
+    about
+    ],
+    tab_names=["Generate Data", "Chat Demonstration", "About"],
     theme=dark_theme
 )
 
 if __name__ == "__main__":
-    demo.launch()
+    app.launch()
